c13363ec046c1798dfc366a82e108bd562fcb7df
alexandersorokin <asorokin@skbkontur.ru>
2021-01-22 13:59:32 +0500

    Test cases are unified

---
+++ Kontur.Tests.Options/Instantiation/Create_Via_Non_Generic/NonNullable_Should.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using FluentAssertions;
+using Kontur.Options;
+using Kontur.Options.Unsafe;
+using NUnit.Framework;
+
+namespace Kontur.Tests.Options.Instantiation.Create_Via_Non_Generic
+{
+    [TestFixture(10)]
+    [TestFixture("bar")]
+    internal class NonNullable_Should<T>
+    {
+        private readonly T value;
+
+        public NonNullable_Should(T value)
+        {
+        this.value = value;
+        }
+
+        private static TestCaseData Create(Func<T, Option<T>> optionFactory, bool hasValue)
+        {
+            return new TestCaseData(optionFactory).Returns(hasValue);
+        }
+
+        private static readonly TestCaseData[] Cases =
+        {
+            Create(_ => Option.None<T>(), false),
+            Create(Option.Some, true),
+        };
+
+        [TestCaseSource(nameof(Cases))]
+        public bool Store_HasValue(Func<T, Option<T>> optionFactory)
+        {
+            var option = optionFactory(value);
+
+            return option.HasSome;
+        }
+
+        [Test]
+        public void Store_Value()
+        {
+            var option = Option.Some(value);
+
+            var result = option.GetOrThrow();
+
+            result.Should().Be(value);
+        }
+    }
+}

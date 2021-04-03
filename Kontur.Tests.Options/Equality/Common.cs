8ca79233485c881749420571ae51f362e757508b
alexandersorokin <asorokin@skbkontur.ru>
2021-04-03 14:37:22 +0500

    Redundant cases in equals is removed

--- Kontur.Tests.Options/Equality/Common.cs
+++ Kontur.Tests.Options/Equality/Common.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Kontur.Options;
 using NUnit.Framework;
 
@@ -6,31 +7,45 @@ namespace Kontur.Tests.Options.Equality
 {
     internal static class Common
     {
-        internal static TestCaseData Create<TValue1, TValue2>(Option<TValue1> option1, Option<TValue2> option2)
+        private static TestCaseData Create<TValue1, TValue2>(Option<TValue1> option1, Option<TValue2> option2)
         {
             return new(option1, option2);
         }
 
+        private static TestCaseData CreateEqual<TValue>(Option<TValue> option)
+        {
+            return Create(option, option);
+        }
+
         internal static IEnumerable<TestCaseData> CreateEqualsCases()
         {
-            yield return Create(Option.None<string>(), Option.None<string>());
-            yield return Create(Option.None<int>(), Option.None<int>());
-            yield return Create(Option.Some(2), Option.Some(2));
-            yield return Create(Option.Some<string?>(null), Option.Some<string?>(null));
-            yield return Create(Option.Some("hello"), Option.Some("hello"));
+            yield return CreateEqual(Option.None<string>());
+            yield return CreateEqual(Option.None<int>());
+            yield return CreateEqual(Option.Some(2));
+            yield return CreateEqual(Option.Some<string?>(null));
+            yield return CreateEqual(Option.Some("hello"));
+        }
+
+        private static IEnumerable<TestCaseData> CreateNotEqual<TValue1, TValue2>(Option<TValue1> option1, Option<TValue2> option2)
+        {
+            yield return new(option1, option2);
+            yield return new(option2, option1);
+        }
+
+        private static IEnumerable<IEnumerable<TestCaseData>> CreateNonEqualsCaseTemplates()
+        {
+            yield return CreateNotEqual(Option.None<string>(), Option.None<int>());
+
+            yield return CreateNotEqual(Option.None<int>(), Option.Some(1));
+
+            yield return CreateNotEqual(Option.Some(1), Option.Some(2));
+            yield return CreateNotEqual(Option.Some("hello"), Option.Some("hi"));
+            yield return CreateNotEqual(Option.Some(2), Option.Some(2.0));
         }
 
         internal static IEnumerable<TestCaseData> CreateNonEqualsCases()
         {
-            yield return Create(Option.None<string>(), Option.None<int>());
-            yield return Create(Option.None<int>(), Option.None<string>());
-
-            yield return Create(Option.None<int>(), Option.Some(1));
-            yield return Create(Option.Some(1), Option.None<int>());
-            yield return Create(Option.Some(1), Option.Some(2));
-            yield return Create(Option.Some("hello"), Option.Some("hi"));
-
-            yield return Create(Option.Some(2), Option.Some(2.0));
+            return CreateNonEqualsCaseTemplates().SelectMany(c => c);
         }
     }
 }

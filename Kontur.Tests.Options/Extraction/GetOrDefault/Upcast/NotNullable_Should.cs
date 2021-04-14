2de4b16c3287a2d348e50bbd438244d4e6b0269c
alexandersorokin <asorokin@skbkontur.ru>
2021-04-14 13:52:12 +0500

    GetOrDefault Upcast test cases are added

---
+++ Kontur.Tests.Options/Extraction/GetOrDefault/Upcast/NotNullable_Should.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Kontur.Options;
+using Kontur.Options.Unsafe;
+using NUnit.Framework;
+
+namespace Kontur.Tests.Options.Extraction.GetOrDefault.Upcast
+{
+    [TestFixture]
+    internal class NotNullable_Should
+    {
+        private static TestCaseData CreateCase(Option<Child> option, Base? result)
+        {
+            return new TestCaseData(option).Returns(result);
+        }
+
+        private static IEnumerable<TestCaseData> GetCases()
+        {
+            yield return CreateCase(Option<Child>.None(), null);
+
+            var child = new Child();
+            yield return CreateCase(Option<Child>.Some(child), child);
+        }
+
+        [TestCaseSource(nameof(GetCases))]
+        public Base? Process_Option(Option<Child> option)
+        {
+            return option.GetOrDefault<Base>();
+        }
+    }
+}

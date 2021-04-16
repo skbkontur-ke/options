53980c89a5efb8cad52146cf3a18612de47481c8
alexandersorokin <asorokin@skbkontur.ru>
2021-04-17 01:46:38 +0500

    Extensions are splitted

---
+++ Kontur.Options/Extensions/Unsafe/EnsureEnsureNoneExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kontur.Options.Unsafe
+{
+    public static class EnsureEnsureNoneExtensions
+    {
+        public static void EnsureNone<TValue>(this Option<TValue> option)
+        {
+            option.OnSome(() => throw new InvalidOperationException($"{option} has value"));
+        }
+    }
+}
--- Kontur.Options/Extensions/Unsafe/EnsureHasValueExtensions.cs
+++ Kontur.Options/Extensions/Unsafe/EnsureHasValueExtensions.cs
@@ -2,16 +2,11 @@
 
 namespace Kontur.Options.Unsafe
 {
-    public static class EnsureExtensions
+    public static class EnsureHasValueExtensions
     {
         public static void EnsureHasValue<TValue>(this Option<TValue> option)
         {
             option.OnNone(() => throw new InvalidOperationException($"No value in {option}"));
-        }
-
-        public static void EnsureNone<TValue>(this Option<TValue> option)
-        {
-            option.OnSome(() => throw new InvalidOperationException($"{option} has value"));
         }
     }
 }

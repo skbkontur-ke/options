ae73cd5e221e7bb84668a909eb913891ab2b55df
alexandersorokin <asorokin@skbkontur.ru>
2021-04-24 20:14:40 +0500

    Interface is used in all Option usage cases apart from LINQ query syntax and methods in Option itself (TryGet, Map, some of Match)

--- Kontur.Options/Extensions/Unsafe/EnsureEnsureNoneExtensions.cs
+++ Kontur.Options/Extensions/Unsafe/EnsureEnsureNoneExtensions.cs
@@ -4,22 +4,22 @@ namespace Kontur.Options.Unsafe
 {
     public static class EnsureEnsureNoneExtensions
     {
-        public static void EnsureNone<TValue>(this Option<TValue> option, Func<TValue, Exception> exceptionFactory)
+        public static void EnsureNone<TValue>(this IOption<TValue> option, Func<TValue, Exception> exceptionFactory)
         {
             option.OnSome(value => throw exceptionFactory(value));
         }
 
-        public static void EnsureNone<TValue>(this Option<TValue> option, Func<Exception> exceptionFactory)
+        public static void EnsureNone<TValue>(this IOption<TValue> option, Func<Exception> exceptionFactory)
         {
             option.EnsureNone(_ => exceptionFactory());
         }
 
-        public static void EnsureNone<TValue>(this Option<TValue> option, Exception exception)
+        public static void EnsureNone<TValue>(this IOption<TValue> option, Exception exception)
         {
             option.EnsureNone(() => exception);
         }
 
-        public static void EnsureNone<TValue>(this Option<TValue> option)
+        public static void EnsureNone<TValue>(this IOption<TValue> option)
         {
             option.EnsureNone(new ValueExistsException($"{option} has value"));
         }
--- Kontur.Options/Extensions/Unsafe/EnsureHasValueExtensions.cs
+++ Kontur.Options/Extensions/Unsafe/EnsureHasValueExtensions.cs
@@ -4,17 +4,17 @@ namespace Kontur.Options.Unsafe
 {
     public static class EnsureHasValueExtensions
     {
-        public static void EnsureHasValue<TValue>(this Option<TValue> option, Func<Exception> exceptionFactory)
+        public static void EnsureHasValue<TValue>(this IOption<TValue> option, Func<Exception> exceptionFactory)
         {
             option.OnNone(() => throw exceptionFactory());
         }
 
-        public static void EnsureHasValue<TValue>(this Option<TValue> option, Exception exception)
+        public static void EnsureHasValue<TValue>(this IOption<TValue> option, Exception exception)
         {
             option.EnsureHasValue(() => exception);
         }
 
-        public static void EnsureHasValue<TValue>(this Option<TValue> option)
+        public static void EnsureHasValue<TValue>(this IOption<TValue> option)
         {
             option.EnsureHasValue(new ValueMissingException($"No value in {option}"));
         }
